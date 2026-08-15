5e2724a3c92ed1e9dcf21be6e9a64bae5699e959
sumity29 <enc.sumitpal29@gmail.com>
2026-08-15 15:36:36 +0530

    adding  more code

--- Request And Response/user.js
+++ Request And Response/user.js
@@ -6,6 +6,18 @@ const server = http.createServer((req, res)=>{
     res.setHeader('Content-Type', 'text/html');
     res.write('<html>');
     res.write('<head><title>Half Coding</title></head>')
+
+    if(req.url === '/'){
+        res.write('<h1>Welcome to HomePage</h1>')
+        return res.end();
+    } else if(req.url.toLocaleLowerCase() === '/products'){
+        res.write('<h1>Products</h1>');
+        return res.end();
+    }
+
+    res.write('<body><h1>Like | Share | Subscribe</h1></body>');
+    res.write('</html>');
+    return res.end();
 });
 
 const PORT = 3000;
